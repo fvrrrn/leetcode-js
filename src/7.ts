61eb171be5b9a6e764fddff1052dbebbce35164c
fvrrrn <boris.chernystrand@gmail.com>
2023-09-22 18:30:55 +0300

    Add reverse integer solution 2

--- src/7.ts
+++ src/7.ts
@@ -18,21 +18,23 @@
 // -2^31 <= x <= 2^31 - 1
 
 export const reverse = (x: number): number => {
-  let result = 0
-  const length = ((Math.log(Math.abs(x) + 1) * 0.43429448190325176) | 0) + 1
-  for (
-    let divider = 10, multiplier = Math.pow(10, length - 1);
-    multiplier >= 1;
-    multiplier /= 10
-  ) {
-    const d = x % divider
-    x -= d
-    x /= divider
-    const m = d * multiplier
-    result += m
+  const minInt = -Math.pow(2, 31)
+  const maxInt = -minInt - 1
+
+  const isNegative = x < 0
+  let reversed = 0
+  if (isNegative) {
+    x = -x
   }
 
-  if (result > 2147483647 || result < -2147483648) return 0
+  while (x > 0) {
+    const remainder = x % 10
+    reversed = reversed * 10 + remainder
+    x = Math.floor(x / 10)
+  }
 
-  return result
+  if (reversed > maxInt) {
+    return 0
+  }
+  return isNegative ? -reversed : reversed
 }
