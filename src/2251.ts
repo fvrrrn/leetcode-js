229b5e56cf533f0f6da953d724f3e82bfa1a0f15
fvrrrn <boris.chernystrand@gmail.com>
2023-10-11 08:46:24 +0300

    Add flowers in bloom visited

--- src/2251.ts
+++ src/2251.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // 2251. Number of Flowers in Full Bloom
 // You are given a 0-indexed 2D integer array flowers, where flowers[i] = [starti, endi] means the ith flower will be in full bloom from starti to endi (inclusive). You are also given a 0-indexed integer array people of size n, where people[i] is the time that the ith person will arrive to see the flowers.
 // Return an integer array answer of size n, where answer[i] is the number of flowers that are in full bloom when the ith person arrives.
@@ -19,16 +21,41 @@
 // 1 <= people.length <= 5 * 104
 // 1 <= people[i] <= 109
 
+// Intuition
+// The number of blooming flowers at any given time is determined by how many flowers have started blooming but have not yet finished. For a given time t, the number that have started blooming is every entry of flowers where flowers[0] <= t. Similarly, the number that have finished is flowers[1] < t. The difference between these two is the number of flowers blooming at t.
+// Approach
+// By sorting the starting times and ending times in flowers separately, we can use a binary search to determine how many flowers have started blooming and how many have finished blooming.
+// Note: because the inequality for starting is <= while ending is <, we can either implement extra logic in the binary search to handle this, or we can just increment every end value by 1 and use <= for both.
+// Additionally, we need a binary search that for an array of length n will return n if the target is bigger than every element in the array. To do this, we'll set the bounds to [0, n] instead of [0, n - 1] and compare our pointers via < rather than <= so we are guaranteed to break the loop when low = high = n.
+// Complexity
+// Time complexity: O((m+n)log⁡n)
+// Space complexity: O(n)
+
+const search = (arr: Uint32Array, target: number): number => {
+  let low = 0
+  let high = arr.length
+  while (low < high) {
+    const mid = ~~((low + high) / 2)
+    if (arr[mid] > target) {
+      high = mid
+    } else {
+      low = mid + 1
+    }
+  }
+  return low
+}
+
 export const fullBloomFlowers = (
   flowers: number[][],
   people: number[],
 ): number[] => {
-  const result = Array(people.length).fill(0)
-  for (let i = 0; i < people.length; i++) {
-    for (let j = 0; j < flowers.length; j++) {
-      if (people[i] >= flowers[j][0] && people[i] <= flowers[j][1]) result[i]++
-    }
+  const starts = new Uint32Array(flowers.length)
+  const ends = new Uint32Array(flowers.length)
+  for (let i = 0; i < flowers.length; i++) {
+    starts[i] = flowers[i][0]
+    ends[i] = flowers[i][1] + 1
   }
-
-  return result
+  starts.sort()
+  ends.sort()
+  return people.map((t) => search(starts, t) - search(ends, t))
 }
