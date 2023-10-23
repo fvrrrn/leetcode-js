2995f14f8b10de89b98fab881c77c6e3835be438
fvrrrn <boris.chernystrand@gmail.com>
2023-10-23 12:31:18 +0300

    Add solution

---
+++ __tests__/14.test.ts
@@ -0,0 +1,15 @@
+import { longestCommonPrefix } from '../src/14.js'
+
+// it('returns "fl" with strs=["flower","flow","flight"]', () => {
+//   expect(longestCommonPrefix(['flower', 'flow', 'flight'])).toEqual('fl')
+// })
+
+// it('returns "" with strs=["dog","racecar","car"]', () => {
+//   expect(longestCommonPrefix(['dog', 'racecar', 'car'])).toEqual('')
+// })
+
+it('returns "flower" with strs=["flower","flower","flower","flower"]', () => {
+  expect(longestCommonPrefix(['flower', 'flower', 'flower', 'flower'])).toEqual(
+    'flower',
+  )
+})
